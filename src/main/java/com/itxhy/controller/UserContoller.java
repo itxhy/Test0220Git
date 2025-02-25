bcfea7726795d1b4cdf0f83f9403cc2ea798dfb6
itxhy <2868458414@qq.com>
2025-02-25 13:06:58 +0800

    add UserController

---
+++ src/main/java/com/itxhy/controller/UserContoller.java
@@ -0,0 +1,29 @@
+package com.itxhy.controller;
+
+import com.itxhy.data.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserContoller {
+    @PostMapping("/add")
+    public void addUser(){
+
+    }
+
+    @PostMapping("/update")
+    public void updateUser(){
+
+    }
+    @DeleteMapping("/delete")
+    public void deleteUser(){
+
+    }
+    @GetMapping("/find")
+    public List<User> findUser(){
+
+        return null;
+    }
+}
---
+++ src/main/java/com/itxhy/data/User.java
@@ -0,0 +1,10 @@
+package com.itxhy.data;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    public String username;
+    public String password;
+}
