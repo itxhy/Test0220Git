0417cbbae465549fd0406f47099279d2c10cab8a
itxhy <2868458414@qq.com>
2025-02-25 16:56:22 +0800

    create getSex()

--- src/main/java/com/itxhy/controller/Test01Controller.java
+++ src/main/java/com/itxhy/controller/Test01Controller.java
@@ -1,6 +1,7 @@
 package com.itxhy.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class Test01Controller {
     @RequestMapping("/testHi")
     public String testHello(){
         return  "hi gitHub";
+    }
+
+    @RequestMapping(value = "/getSex",method = RequestMethod.GET)
+    public void getSex(){
+
     }
 }
